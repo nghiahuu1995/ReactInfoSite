e077736db58d3c9c2964a74bd23e783611c6bf01
Nghia Nguyen <ainzynguyen95@gmail.com>
2024-05-08 23:54:27 -0700

    first commit

---
+++ components/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar.js";
+import Main from "./components/Main.js";
+
+export default function App() {
+    return (
+        <div className="container">
+            <NavBar></NavBar>
+            <Main></Main>
+        </div>
+    );
+}
---
+++ components/NavBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTw3HApB4bsvabXW3L14cV-LhFo0L71QmEESJN3vW9Ow&s"
+                alt=""
+                className="mav--icon"
+            />
+            <h3 className="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+    );
+}
